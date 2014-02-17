d4bcdfa36b20425384155e221416cdb791a59eef
imsong <hkssong@gmail.com>
2014-02-17 13:35:44 +0900

    test

--- app.js
+++ app.js
@@ -5,16 +5,18 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var chart = require('./routes/chart');
 var http = require('http');
 var path = require('path');
+var mysql = require('mysql');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -22,15 +24,24 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+//connect to mysql database
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database : 'test'
+});
+connection.connect();
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/chart', chart.first);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
